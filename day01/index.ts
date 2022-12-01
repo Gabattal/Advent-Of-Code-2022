ec23a8be2d95be83ff9589b3a5adb17673d0012f
Gabriel Attal <gab-attal@hotmail.fr>
2022-12-01 18:12:18 +0100

    lint day01

--- day01/index.ts
+++ day01/index.ts
@@ -2,14 +2,14 @@ import {parseInt} from "lodash";
 
 function run1(input: Array<string>) {
 
-    const splittedInput = input.map((input)=>input.split(' '))
+    const splittedInput = input.map((input) => input.split(' '))
     let max = 0;
     let total = 0;
-    for(const line of splittedInput){
-        if(line[0]!== ""){
-            total+= parseInt(line[0])
+    for (const line of splittedInput) {
+        if (line[0] !== "") {
+            total += parseInt(line[0])
         } else {
-            if(max<total){
+            if (max < total) {
                 max = total
             }
             total = 0
@@ -21,21 +21,20 @@ function run1(input: Array<string>) {
 
 function run2(input: Array<string>) {
     let arrayOfNumber: Array<number> = [];
-
-    const splittedInput = input.map((input)=>input.split(' '))
+    const splittedInput = input.map((input) => input.split(' '))
     let total = 0;
-    for(const line of splittedInput){
-        if(line[0]!== ""){
-            total+= parseInt(line[0])
+    for (const line of splittedInput) {
+        if (line[0] !== "") {
+            total += parseInt(line[0])
         } else {
             arrayOfNumber.push(total)
             total = 0
         }
     }
-    const sortedArray = arrayOfNumber.sort((a,b)=>b-a)
-    const result = sortedArray[0]+sortedArray[1]+sortedArray[2]
+    const sortedArray = arrayOfNumber.sort((a, b) => b - a)
+    const result = sortedArray[0] + sortedArray[1] + sortedArray[2]
     console.log(result)
 }
 
-export {run1,run2}
+export {run1, run2}
 
