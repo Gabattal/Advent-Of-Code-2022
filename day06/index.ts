0323a8924b03fec85dbabf5fb3acdae7d3682704
Gabriel Attal <gab-attal@hotmail.fr>
2022-12-07 02:24:43 +0100

    day 06

--- day06/index.ts
+++ day06/index.ts
@@ -1,12 +1,12 @@
 function run1(input: Array<string>) {
     const values: Array<string> = []
 
-    for(let i=0 ; i<input[0].length ;i++){
-        if(values.length>3){
+    for (let i = 0; i < input[0].length; i++) {
+        if (values.length > 3) {
             const multiple = new Set(values).size !== values.length
-            if(!multiple){
+            if (!multiple) {
                 console.log(i)
-                break
+                break;
             }
             values.shift()
         }
@@ -16,12 +16,12 @@ function run1(input: Array<string>) {
 
 function run2(input: Array<string>) {
     const values: Array<string> = []
-    for(let i=0 ; i<input[0].length ;i++){
-        if(values.length>13){
+    for (let i = 0; i < input[0].length; i++) {
+        if (values.length > 13) {
             const multiple = new Set(values).size !== values.length
-            if(!multiple){
+            if (!multiple) {
                 console.log(i)
-                break
+                break;
             }
             values.shift()
         }
