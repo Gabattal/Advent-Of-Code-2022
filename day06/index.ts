dfd54bc0b5c3785d5ab74605a82676f0a829790b
Gabriel Attal <gab-attal@hotmail.fr>
2022-12-07 02:19:25 +0100

    day06

---
+++ day06/index.ts
@@ -0,0 +1,33 @@
+function run1(input: Array<string>) {
+    const values: Array<string> = []
+
+    for(let i=0 ; i<input[0].length ;i++){
+        if(values.length>3){
+            const multiple = new Set(values).size !== values.length
+            if(!multiple){
+                console.log(i)
+                break
+            }
+            values.shift()
+        }
+        values.push(input[0][i])
+    }
+}
+
+function run2(input: Array<string>) {
+    const values: Array<string> = []
+    for(let i=0 ; i<input[0].length ;i++){
+        if(values.length>13){
+            const multiple = new Set(values).size !== values.length
+            if(!multiple){
+                console.log(i)
+                break
+            }
+            values.shift()
+        }
+        values.push(input[0][i])
+    }
+}
+
+export {run1, run2}
+
